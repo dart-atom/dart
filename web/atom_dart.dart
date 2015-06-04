c53b9d18208fccb16dbb63d95605b2e8e98db757
Luke Church <lukechurch@google.com>
2015-06-04 15:38:13 -0700

    Add logging support

--- web/atom_dart.dart
+++ web/atom_dart.dart
@@ -2,23 +2,38 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'atom.dart';
+import 'package:atom_dart/atom.dart';
+import 'package:logging/logging.dart';
+
+Logger _logger = new Logger("AtomDartPackage");
 
 main() {
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
   registerPackage(new AtomDartPackage());
+
 }
 
 class AtomDartPackage extends AtomPackage {
   void packageActivated([Map state]) {
+    _logger.fine('AtomDartPackage.packageActivated, state: $state');
+
     //atom.beep();
 
     atom.commands.add('atom-workspace', 'dart-lang:hello-world', (e) {
       atom.notifications.addInfo(
         'Hello world from dart-lang!', options: {'detail': 'Foo bar.'});
+      _logger.info("Hello notification from dart-lang");
     });
   }
 
   void packageDeactivated() {
+    _logger.fine('AtomDartPackage.packageDeactivated');
+
 
   }
 }
