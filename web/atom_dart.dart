5b17f0829b9fcdb11415fa38f4b768e0f4f46cea
Luke Church <lukechurch@google.com>
2015-06-04 15:48:51 -0700

    Update package ref

--- web/atom_dart.dart
+++ web/atom_dart.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:atom_dart/atom.dart';
+import 'package:atom_dart/atom/atom.dart';
 import 'package:logging/logging.dart';
 
 Logger _logger = new Logger("AtomDartPackage");
