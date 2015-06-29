718de684bba702bcfcc7f624cdcf26d9603899fa
Arron Washington <arron@theradicaledwards.com>
2015-06-29 17:47:11 -0400

    Remove unnecessary TODO

--- lib/linter/dart_linter_provider.dart
+++ lib/linter/dart_linter_provider.dart
@@ -1,8 +1,7 @@
 part of linter;
 
 /// This only class exists to provide linting information to atomlinter/linter.
-class DartLinterProvider extends LinterProvider {
-  // TODO: Options are 'file' and 'project' scope, and lintOnFly true or false.
+class DartLinterProvider extends LinterProvider {  
   DartLinterProvider()
       : super(grammarScopes: ['source.dart'], scope: 'project');
 
