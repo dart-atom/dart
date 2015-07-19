76d529e0a797417305b4d8a8887e45c42efda732
Devon Carew <devoncarew@gmail.com>
2015-07-19 06:01:56 -0700

    tweak to logging

--- lib/analysis_server.dart
+++ lib/analysis_server.dart
@@ -373,8 +373,8 @@ class _AnalysisServerWrapper extends Server {
   void setup() {
     server.setSubscriptions(['STATUS']);
     // TODO: Remove once 1.12.0 is shipped as stable.
-    // Uncaught Uncaught Error: [RequestError code: INVALID_PARAMETER,
-    // message: Invalid parameter 'params.options.enableAsync'. Expected to be bool.]
+    // [RequestError code: INVALID_PARAMETER, message: Invalid parameter
+    // 'params.options.enableAsync'. Expected to be bool.]
     // https://github.com/dart-lang/sdk/issues/23863
     analysis.updateOptions(new AnalysisOptions(
       enableAsync: true,
@@ -385,7 +385,7 @@ class _AnalysisServerWrapper extends Server {
       generateLints: true,
       enableNullAwareOperators: true
     ));
-    server.getVersion().then((v) => _logger.info('analysis server version: ${v.version}'));
+    server.getVersion().then((v) => _logger.info('version ${v.version}'));
     server.onStatus.listen((ServerStatus status) {
       if (status.analysis != null) {
         analyzing = status.analysis.isAnalyzing;
