52c6d48030ee4285de2616114d22c35df65a27b0
Devon Carew <devoncarew@gmail.com>
2015-12-15 12:07:22 -0800

    update plugin settings description

--- lib/plugin.dart
+++ lib/plugin.dart
@@ -383,9 +383,9 @@ class AtomDartPackage extends AtomPackage {
       },
       'debugAnalysisServer': {
         'title': 'Start the analysis server with debug flags.',
-        'description': 'This is for plugin development only! The analysis server '
-            'will be started with the observatory port and AS diagnostics ports '
-            'turned on. A restart is required.',
+        'description':
+          'Start the analysis server with its diagnostics port enabled. A '
+          'restart is required.',
         'type': 'boolean',
         'default': false,
         'order': 11
