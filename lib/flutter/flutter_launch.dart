7773312e47b9e2197d6f0e1344aa892d0111a5b8
Seth Ladd <sethladd@gmail.com>
2016-02-26 10:13:54 -0800

    tweak from review

--- lib/flutter/flutter_launch.dart
+++ lib/flutter/flutter_launch.dart
@@ -70,7 +70,8 @@ class FlutterLaunchType extends LaunchType {
   }
 
   String getDefaultConfigText() {
-    return 'checked: true\ndebug: true\nroute:\n# additional args for the flutter run command\nargs:\n';
+    return 'checked: true\ndebug: true\nroute:\n'
+           '# Additional args for the flutter run command:\nargs:\n';
   }
 
   Future _killLastLaunch() {
