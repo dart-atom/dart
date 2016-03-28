f31d9308162ddc7f2a20b268727555a1a89de07a
Devon Carew <devoncarew@google.com>
2016-03-28 12:45:54 -0700

    improve our support for BUILD files

--- spec/all-spec.dart
+++ spec/all-spec.dart
@@ -2,13 +2,13 @@
 //import '_spec/sample-spec.dart' as sample_spec;
 
 import 'flutter/launch_flutter_test.dart' as launch_flutter_test;
+import 'projects_test.dart' as projects_test;
 import 'sdk_test.dart' as sdk_test;
-
-// TODO: Move tests over from smoketest.dart.
 
 main() {
   //animals_test.register();
   //sample_spec.main();
   launch_flutter_test.register();
+  projects_test.register();
   sdk_test.register();
 }
